5b71fd9884bcd3dbd7814fe74e1fddece167bc62
csmahmud99 <csmahmud.2021@gmail.com>
2023-05-15 15:47:19 +0600

    Simple CRUD Client is initializing

---
+++ src/components/Update.jsx
@@ -0,0 +1,48 @@
+import { useLoaderData } from 'react-router-dom';
+
+const Update = () => {
+    const loadedUser = useLoaderData();
+
+    const handleUpdate = event => {
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const email = form.email.value;
+
+        // console.log(name, email);
+
+        const updatedUser = { name, email };
+
+        console.log(updatedUser);
+
+        fetch(`http://localhost:5000/users/${loadedUser._id}`, {
+            method: "PUT",
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(updatedUser)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if(data.modifiedCount > 0) {
+                    alert("User is updated successfully");
+                }
+            });
+    };
+
+    return (
+        <div>
+            <h3>Update Information of : {loadedUser.name}</h3>
+            <form onSubmit={handleUpdate}>
+                <input type="text" name="name" id="name" defaultValue={loadedUser?.name} />
+                <br />
+                <input type="email" name="email" id="email" defaultValue={loadedUser?.email} />
+                <br />
+                <input type="submit" value="Update" />
+            </form>
+        </div>
+    );
+};
+
+export default Update;
